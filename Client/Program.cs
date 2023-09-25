b970f10b7b04ba24ffb3065ac2bdba70af4888a5
Luke Kellet <lukejameskellet@hotmail.com>
2023-09-25 14:13:53 +0100

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket client = null;
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddr = null;
+            try
+            {
+                Console.WriteLine("Welcome to Socket Client Starter ***");
+                Console.WriteLine("Please enter the server IP address: ");
+                string strIP = Console.ReadLine();
+                Console.WriteLine("Please enter the server Port: ");
+                string strPort = Console.ReadLine();
+                int nPort = 0;
+
+                if (strIP == " ")
+                {
+                    strIP = "127.0.0.1";
+                }
+                if (strPort == " ")
+                {
+                    strPort = "25000";
+                }
+
+                if (!IPAddress.TryParse(strIP, out ipaddr))
+                {
+                    Console.WriteLine("Invalid IP address format");
+                    return;
+                }
+
+                if (!int.TryParse(strPort.Trim(), out nPort))
+                {
+                    Console.WriteLine("Invalid port number");
+                    return;
+                }
+
+                if (nPort <= 0 || nPort > 65535)
+                {
+                    Console.WriteLine("Port number must be between 0 and 65535");
+                    return;
+                }
+
+                System.Console.WriteLine(string.Format("IPAddress: {0} - Port: {1}", ipaddr.ToString(), nPort));
+                client.Connect(ipaddr, nPort);
+
+                Console.WriteLine("Connected to the server, type text and press enter to send it to the server. Type <EXIT> to close connection and exit");
+                string inputCommand = string.Empty;
+
+                while (true)
+                {
+                    inputCommand = Console.ReadLine();
+                    if (inputCommand == "<EXIT>")
+                    {
+                        break;
+                    }
+                    byte[] buffSend = Encoding.ASCII.GetBytes(inputCommand);
+                    client.Send(buffSend);
+                    byte[] buffReceived = new byte[128];
+                    int nRecv = client.Receive(buffReceived);
+                    Console.WriteLine("Data received: {0}", Encoding.ASCII.GetString(buffReceived, 0, nRecv));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (client != null)
+                {
+                    if (client.Connected)
+                    {
+                        client.Shutdown(SocketShutdown.Both);
+                    }
+                    client.Close();
+                    client.Dispose();
+                }
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an object of socket class
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //Create an object of an IP Address.socket listening on any ip address.
+            IPAddress ipaddr = IPAddress.Any;
+            //Define IP End Point
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 25000);
+            //Bind socket to end point
+            try
+            {
+                listenerSocket.Bind(ipep);
+                //Call Listen Method
+                listenerSocket.Listen(5);
+                //Call Accept Method
+                Socket client = listenerSocket.Accept();
+                //Print out client ip end point
+                Console.WriteLine("Client Connected " + client.ToString() + " " + client.RemoteEndPoint.ToString());
+                //Define buffer
+                byte[] buff = new byte[128];
+                //Define number of received bytes
+                int numberOfReceivedBytes = 0;
+                while (true)
+                {
+                    numberOfReceivedBytes = client.Receive(buff);
+                    Console.WriteLine("Number of received bytes: " + numberOfReceivedBytes);
+                    //Convert from byte array to ascii, using encoding.ascii.getstring
+                    Console.WriteLine("Data sent by client: " + Encoding.ASCII.GetString(buff, 0, numberOfReceivedBytes));
+                    string receivedText = Encoding.ASCII.GetString(buff, 0, numberOfReceivedBytes);
+                    //print out received text
+                    Console.WriteLine("Data sent by client: " + receivedText);
+                    //Input response to send to client
+                    Console.WriteLine("Please enter the response to send to client: ");
+                    string responseText = Console.ReadLine();
+                    //Send back to client
+                    buff = Encoding.ASCII.GetBytes(responseText);
+                    client.Send(buff);
+                    if (receivedText == "x")
+                    {
+                        break;
+                    }
+                    Array.Clear(buff, 0, buff.Length);
+                    numberOfReceivedBytes = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
